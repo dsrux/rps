0b3c0513d6074a19a6e27a21388fb587c8fa521f
dsrux <dsrux777777@gmail.com>
2019-05-02 18:27:51 -0600

    Setup

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace rps
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Console.WriteLine("Welcome to Rock Paper Scissors");
+      Random rng = new Random
+
+
+    }
+  }
+}
